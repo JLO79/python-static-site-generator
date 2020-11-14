c2494cf75be0bb036fdf3847cdea0a4ca26ec2f5
jalord <jalord@deby.home>
2020-11-14 16:32:49 +0100

    Completed module

--- ssg.py
+++ ssg.py
@@ -1,19 +1,19 @@
 import typer
 from ssg.site import Site
-"""
-We'll call this function main. It should accept two keyword arguments: source
-with a default value of "content", and dest with a default value of "dist".
+import ssg.parsers
 
-In the body of the main function, create a dictionary called config.
-Add two key value pairs to config: "source" set to source,
-and "dest" set to dest.
+"""
 """
 
 def main(source="content", dest="dist"):
     config = {
         "source": source,
-        "dest": dest
+        "dest": dest,
+        "parsers": [
+        ssg.parsers.ResourceParser(),
+        ]
         }
+
     print(config)
     Site(**config).build()
 
